0d645e27c2bdd4a97bfe546be7e1aefaf5bbc9a6
thidayu <tanthidayu_0000@hotmail.com>
2022-01-18 16:34:33 +0700

    lab 17.1 finished

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -13,22 +13,70 @@ void findRowSum(const double *,double *,int,int);
 void findColSum(const double *,double *,int,int);
 
 int main(){
-	srand(time(0));
-	const int N = 6, M = 8;
-	double data[N][M] = {};
-	double sum1[N] = {};
-	double sum2[M] = {};
-	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
-	cout << "---------------------------------------------\n";
-	
-	findRowSum(dPtr,sum1,N,M);
-	showData(sum1,N,1);
-	
-	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,sum2,N,M); 
-	showData(sum2,1,M);
+ srand(time(0));
+ const int N = 6, M = 8;
+ double data[N][M] = {};
+ double sum1[N] = {};
+ double sum2[M] = {};
+ double *dPtr = data[0];
+ randData(dPtr,N,M);
+ showData(dPtr,N,M);
+ 
+ cout << "---------------------------------------------\n";
+ 
+ findRowSum(dPtr,sum1,N,M);
+ showData(sum1,N,1);
+ 
+ cout << "---------------------------------------------\n";
+ 
+ findColSum(dPtr,sum2,N,M); 
+ showData(sum2,1,M);
+}
+
+void showData(double *x, int N, int M)
+{
+ cout << fixed << setprecision(2);
+ for(int i = 0; i < N*M; i++)
+ {
+  cout << *(x+i);
+  if((i+1)%M == 0 ) cout << endl;
+  else cout << " ";
+ }
+}
+
+void randData(double *x, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(x+i) = (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(const double *x, double *a, int N, int M)
+{
+    int y = 0;
+    for(int i = 0; i < N; i++)
+    {
+        double z = 0;
+        for(int j = 0; j < M; j++)
+        {
+            z += *(x+y);
+            y += 1;
+        }
+        *(a+i) = z;
+    }
+}
+
+void findColSum(const double *x, double *b, int N, int M)
+{
+    int y = 0;
+    for(int j = 0; j < M; j++)
+    {
+        y = j;
+        double z = 0;
+        for(int i = 0; i < N; i++)
+        {
+            z += *(x+y);
+            y += M;
+        }
+        *(b+j) = z;
+    }
 }
